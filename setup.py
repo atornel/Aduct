6e416804fd4ffe2af8f44f3988746352af0604fc
J Arun Mani <49952138+J-Arun-Mani@users.noreply.github.com>
2020-04-26 21:20:47 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name="Aduct",
-    version="1.1.0",
+    version="1.1.0a",
     author="J Arun Mani; Atornel",
     author_email="j.arunmani@protonmail.com",
     description="Aduct lets you make flexible interfaces that can be easily\
