ea3726cfe3af377c419962d88d4f6753ddb56a85
J Arun Mani <j.arunmani@protonmail.com>
2020-04-26 21:58:40 +0530

    Small edits to setup files.

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2020, Atornel"
 author = "J Arun Mani"
 
 # The full version, including alpha/beta/rc tags
-release = "1.0"
+release = "1.1.0"
 master_doc = "index"
 
 # -- General configuration ---------------------------------------------------
